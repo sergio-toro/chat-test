f8abd6a792fcf2e810c204d750660cb8e53406a9
Sergio Toro <yiti007@gmail.com>
2017-04-26 20:19:24 +0200

    display application and adjust root layout to display the app correctly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ChatInterface from './components/ChatInterface'
 
-class App extends Component {
+import './App.css'
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <ChatInterface
+        chattingWith={{ id: 2, nickname: 'Hola mundo!' }}
+        user={{ id: 1, nickname: null }}
+        messages={[
+          { userId: 1, message: 'Hey, what\'s up man? (smile)' },
+          { userId: 2, message: 'Oh it\'s you?' },
+          { userId: 1, message: 'Do you want a beer?' },
+          { userId: 2, message: 'Yep! Where do you want to go? Do you know that new place in the city center?' },
+          { userId: 1, message: 'Green Beer, right?' },
+          { userId: 1, message: 'Sounds cool, let\'s go!' },
+        ]}
+
+        onSendMessage={(data) => console.log('send message', data)}
+        onSendThinkMessage={(data) => console.log('send think message', data)}
+        onSetNick={(data) => console.log('set nickname', data)}
+        onRemoveLast={(data) => console.log('remove last', data)}
+      />
+    )
   }
 }
-
-export default App;
